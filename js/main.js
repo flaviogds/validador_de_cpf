17ae5305491e529e1fc199a32687d352adae2257
Flávio Santos <flavio.gds@gmail.com>
2020-07-27 17:00:44 -0300

    created or js file and an improvement of the validation functions

---
+++ js/main.js
@@ -0,0 +1,67 @@
+function consult() {
+    let input = document.getElementById('input').value;
+
+    if(input=='') {
+        document.getElementById('pass').style.display = 'block';
+        document.getElementById('pass').innerHTML = 'Campo CPF em branco';
+    }
+    else {
+        let result = validation(input);
+
+        if(result){
+            document.getElementById('pass').style.display = 'block';
+            document.getElementById('pass').innerHTML = 'CPF valido';
+        }
+        else{
+            document.getElementById('pass').style.display = 'block';
+            document.getElementById('pass').innerHTML = 'CPF invalido'
+        }
+    }
+}
+
+function validation(input) {
+    let array = input.substring(0, 9);
+    let digits = input.substring(9);
+
+    let soma = 0;
+    let valid = '';
+
+    for(i = 0; i < array.length; i++) {
+        soma = soma + parseInt(array[i])*(10-i);
+    }
+
+    valid = (soma*10)%11 == 10 || (soma*10)%11 == 11 ? 0 : (soma*10)%11;
+
+    if(valid == parseInt(digits[0])) {
+        soma = 0;
+        array += digits.charAt(0);
+
+        for(i = 0; i < array.length; i++) {
+            soma = soma + parseInt(array[i])*(11-i);
+        }
+
+        valid = (soma*10)%11 == 10 || (soma*10)%11 == 11 ? 0 : (soma*10)%11;
+
+        return valid == parseInt(digits[1]);
+    }
+}
+
+
+function validInput() {
+    let input = document.getElementById('input').value;
+
+    let array = input.split('');
+
+    for(i = 0 ; i < array.length ; i++){
+
+        let valid = parseInt(array[i]);
+
+        if(isNaN(valid)) {  
+            document.getElementById('out').style.display = 'block';
+        }
+        else {  
+            document.getElementById('out').style.display = 'none';
+            document.getElementById('pass').style.display = 'none'
+        }
+    }
+}
